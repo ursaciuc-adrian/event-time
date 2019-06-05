d802f0a3761c867f72b1b67bd221791e6583876b
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-06-05 21:47:32 +0300

    Meetup get events and categories + change requests

--- back/src/models/change-request.model.ts
+++ back/src/models/change-request.model.ts
@@ -1,14 +1,38 @@
 import mongoose, { Schema } from 'mongoose';
 
-const ChageRequestSchema = new Schema({
+const ChangeRequestSchema = new Schema({
 	id: {
 		type: mongoose.Schema.ObjectId,
 		index: { unique: true }
 	},
-	problem: {
+	title: {
 		type: String,
-		required: true
+		required: false,
+		trim: true
+	},
+	description: {
+		type: String,
+		required: false,
+		trim: true
+	},
+	location: {
+		type: String,
+		required: false,
+		trim: true
+	},
+	seats: {
+		type: Number,
+		required: false
+	},
+	timestamp: {
+		type: String,
+		required: false,
+		trim: true
+	},
+	checked: {
+		type: Boolean,
+		default: false
 	}
 });
 
-export default mongoose.model('ChageRequest', ChageRequestSchema);
+export default mongoose.model('ChangeRequest', ChangeRequestSchema);
--- back/src/services/events-fetcher.service.ts
+++ back/src/services/events-fetcher.service.ts
@@ -52,3 +52,49 @@ export async function getEventbriteEvents(): Promise<void> {
 		throw err;
 	}
 }
+
+export async function getEventbriteEvents(): Promise<void> {
+	try {
+		const categories = await Category.find({ originName: 'meetup' });
+
+		categories.forEach(async (category) => {
+			try {
+				const response = await request(
+					'https://api.meetup.com/2/open_events?&category=' + category.idOrigin + '&sign=true&photo-host=public&key=352395f2f577c7216632a056757444',
+					{
+						method: 'GET'
+					});
+
+				const body = JSON.parse(response.body);
+
+				body.results.forEach(async (element) => {
+					try {
+						const count = await Event.count({ idOrigin: element.id });
+
+						if (count === 0) {
+							const obj = new Event({
+								idOrigin: element.id,
+								idCategory: category._id,
+								title: element.name,
+								description: element.description,
+								location: element.venue.address_1 != null ? element.venue.address_1 : '',
+								seats: element.rsvp_limit != null ? element.rsvp_limit : 0,
+								coverPhoto: element.photo_url != null ? element.photo_url : '',
+								date: element.start.local // de modificat
+							});
+							// TODO : verifica pe aici 
+
+							await obj.save();
+						}
+					} catch (err) {
+						throw err;
+					}
+				});
+			} catch (err) {
+				throw err;
+			}
+		});
+	} catch (err) {
+		throw err;
+	}
+}
