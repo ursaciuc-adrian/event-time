7a2e9c258b4530f1171b084a3202b5d185e99d09
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-06-11 01:42:21 +0300

    reformat

--- front/scripts/subscribe.js
+++ front/scripts/subscribe.js
@@ -36,11 +36,66 @@ document.querySelector("#subscribe-form").addEventListener("submit", function (e
                 console.log(err);
             })
     });
-
-
-
-
-
-
-
 });
+
+let categoriesContainer = document.getElementById("categories");
+
+fetch("http://localhost:3000/auth/me", {
+        method: "GET",
+        headers: {
+            'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+        }
+    })
+    .then(resp => resp.json())
+    .then(myJson2 => {
+        if (myJson2) {
+            console.log(myJson2.data.id);
+            fetch("http://localhost:3000/categories/unsubscribed?id=" + myJson2.data.id, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (myJson) {
+                    let categories = myJson.data.categories;
+                    let i = 0;
+                    categories.forEach(category => {
+                        let div = document.createElement("div");
+                        let input = document.createElement("input");
+                        input.type = "checkbox";
+                        input.id = category._id;
+                        input.name = "category";
+
+                        let label = document.createElement("label");
+                        label.htmlFor = category._id;
+
+                        let img = document.createElement("img");
+                        img.src = "images/mountains.jpeg";
+                        img.alt = " ";
+
+                        let span = document.createElement("span");
+                        span.innerText = category.name;
+
+                        label.appendChild(img);
+                        label.appendChild(span);
+                        div.appendChild(input);
+                        div.appendChild(label);
+
+                        categoriesContainer.appendChild(div);
+
+                        i++;
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            window.location.href = "index.html";
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
